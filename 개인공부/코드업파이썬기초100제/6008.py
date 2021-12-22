c19da4533f64c039d2cb122d539633b0a70f2d2c
korilla2 <kingdoss@naver.com>
2021-12-22 10:33:37 +0900

    커밋

--- 개인공부/코드업파이썬기초100제/6008.py
+++
@@ -1,7 +0,0 @@
-'''
-6008 : [기초-출력] 출력하기08(py)
-
-print("Hello\nWorld")
-'''
-
-print(r'''print("Hello\nWorld")''')
--- 개인공부/코드업파이썬기초100제/6017.py
+++
@@ -1,9 +0,0 @@
-'''
-6017 : [기초-입출력] 문장 1개 입력받아 3번 출력하기(설명)(py)
-
-computer science computer science computer science
-'''
-
-a = input()
-
-print(f'{a} ' * 3)
