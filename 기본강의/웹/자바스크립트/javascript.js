2e3b32213d46390e64c1a7542582c366178b1419
korilla2 <kingdoss@naver.com>
2021-12-22 17:33:16 +0900

    커밋

---
+++ 기본강의/웹/자바스크립트/javascript.js
@@ -0,0 +1,40 @@
+// // 윤년 구하기
+
+// let year = 2000;
+
+// if (year % 400 == 0) {
+//     console.log('윤년입니다')
+// } else if (year % 100 == 0) {
+//     console.log('윤년이 아닙니다')
+// } else if (year % 4 == 0) {
+//     console.log('윤년입니다')
+// } else {
+//     console.log('윤년이 아닙니다')
+// }
+
+// // 1부터 10까지 반복
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// array = [10, 20, 30, 40]
+// for (let x of array) {
+//     console.log(x)
+// }
+
+// // 1부터 n까지 총합 구하기
+// let n = 20;
+// let s = 0;
+// let i = 1;
+// while (i <= n) {
+//     s += i;
+//     i++;
+// }
+// console.log(s)
