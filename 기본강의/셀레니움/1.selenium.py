14128876a1bd307631dfe22ceadf9e42c2a48340
korilla2 <kingdoss@naver.com>
2022-01-03 17:54:12 +0900

    커밋

---
+++ 기본강의/셀레니움/1.selenium.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Chrome(
+    'C:/Users/BAUM/Desktop/multicampus/TIL/chromedriver.exe')
+
+url = 'http://itempage3.auction.co.kr/DetailView.aspx?itemno=B726462956'
