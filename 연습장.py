afb1d07f205f001b44aba5531f5966097def59ae
korilla2 <kingdoss@naver.com>
2022-01-13 17:51:56 +0900

    커밋

--- 연습장.py
+++ 연습장.py
@@ -1,24 +1,9 @@
-numData = \
-    [
-        [5, 7, -5, 100, 73],
-        [35, 23, 4, 190, 33],
-        [49, 85, 662, 39, 81],
-        [124, -59, 86, 46, 52],
-        [27, 7, 8, 33, -56]
-    ]
+import dart_fss as dart
 
+api_key = 'f4af423987105c23a450a884919dec8cf35b0107'
+dart.set_api_key(api_key=api_key)
 
-for i in range(len(numData)):
-    for j in range(len(numData[i])):
-        if numData[i][j] < 0:
-            numData[i][j] = 0
-        elif numData[i][j] > 100:
-            numData[i][j] = numData[i][j] % 100
-temp = 0
-for j in range(len(numData) - 1):
-    for i in range(len(numData) - 1):
-        a = numData[j][i] + numData[j+1][i] + \
-            numData[j][i+1] + numData[j+1][i+1]
-        if a > temp:
-            temp = a
-print(temp)
+# 2021년 1월 1일부터 2021년 3월 31일까지 검색
+reports = dart.filings.search(bgn_de='20210101', end_de='20210331')
+
+print(reports)
