ae430dcedcab07f1ec1da9ba8b974b9fd062acc5
korilla2 <kingdoss@naver.com>
2022-01-17 18:00:04 +0900

    커밋

--- test4.py
+++ test4.py
@@ -1,3 +1,11 @@
-k = list(map(int, input().split()))
-k = sorted(k)
-print('k', k)
+import urllib.request
+from bs4 import BeautifulSoup
+code = ['005930']
+for c in code:
+    url = "http://comp.fnguide.com/SVO2/ASP/SVD_Invest.asp?pGB=1&gicode=A" + \
+        c + "&cID=&MenuYn=Y&ReportGB=&NewMenuID=105&stkGb=701"
+    f = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(f, 'html.parser')
+    cells = soup.find('tr', {'id': "p_grid1_10"}).find_all('td')
+    for cell in cells:
+        print(cell.string)
--- 연습장2.py
+++ 연습장2.py
@@ -6,12 +6,12 @@ dart.set_api_key(api_key=api_key)
 
 # DART 에 공시된 회사 리스트 불러오기
 corp_list = dart.get_corp_list()
-corp_code = '251270'
+
 # 삼성전자 검색
-samsung = corp_list.find_by_stock_code(stock_code=corp_code)
+samsung = corp_list.find_by_corp_name('SK하이닉스', exactly=True)[0]
 
 # 2012년부터 연간 연결재무제표 불러오기
-fs = samsung.extract_fs(bgn_de='20200101', end_de='20200331')
+fs = samsung.extract_fs(bgn_de='20110101', end_de='20210101')
 
 # 재무제표 검색 결과를 엑셀파일로 저장 ( 기본저장위치: 실행폴더/fsdata )
-print(fs)
+fs.save()
