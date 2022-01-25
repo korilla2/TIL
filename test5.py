af5ff4b56302a9920f32f59af98fd79049b78ca9
korilla2 <kingdoss@naver.com>
2022-01-26 01:15:07 +0900

    커밋

--- test5.py
+++ test5.py
@@ -1,3 +1,6 @@
+from random import *
+from datetime import datetime
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
@@ -8,3 +11,8 @@ a = [[1, 2, 3],
 a = [[1, 2, 3],
      [4, 5, 6],
      [7, 8, 9]]
+
+
+class Matrix(object):
+    def __init__(self):
+        self.matrix = None
