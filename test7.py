82fdf141cf1cbe8811cd81bb593a849f2ca68fe4
korilla2 <kingdoss@naver.com>
2022-02-08 12:03:33 +0900

    커밋

--- test7.py
+++ test7.py
@@ -1,7 +1,16 @@
-result = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+from collections import Counter
+
+result = [[1, 3, 3], [4, 7, 6], [7, 8, 9]]
 
 print(result)
 
 for i in range(len(result)):
     for j in range(len(result)):
         print(result[i][j])
+
+b = sum(result, [])
+a = Counter(b)
+print(a)
+
+sex = a.most_common(3)
+print(sex)
