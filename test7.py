16c2b1ce56cb8658513ecd09ff38a1eb751b4c07
korilla2 <kingdoss@naver.com>
2022-03-07 03:08:28 +0900

    커밋

--- test7.py
+++ test7.py
@@ -1,3 +1,28 @@
-import numpy as np
-a = np.zeros((1, 2000))
-print(a)
+import keyboard
+import time
+
+
+def aaa():
+    try:
+        time.sleep(2)
+        print(1/0)
+    except:
+        return
+
+    try:
+        time.sleep(10)
+        print(1/0)
+    except:
+        return
+
+
+def bbb():
+    print('b')
+
+
+keyboard.add_hotkey('q', aaa)
+keyboard.add_hotkey('w', bbb)
+
+
+while True:
+    keyboard.read_key()
